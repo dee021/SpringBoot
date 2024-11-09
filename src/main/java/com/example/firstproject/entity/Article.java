bc47e0917f7b7704bcdf9d6ccd5e991716aa8524
dee021 <dee21@gachon.ac.kr>
2024-11-09 13:55:06 +0900

    first commit

---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,41 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+// 엔티티 선언
+@Entity
+public class Article {
+    @Id
+    // PK 지정
+    @GeneratedValue
+    // 자동 생성 기능 추가
+    private Long id;
+
+    @Column
+    // 필드 선언
+    private String title;
+
+    @Column
+    private String content;
+
+    public Article(Long id, String title, String content) {
+        this.content = content;
+        this.title = title;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Aricle{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+
+
+}
