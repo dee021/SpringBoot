87ae68afaf6da9ed7fa48189f4fb798970308d18
dee021 <dee21@gachon.ac.kr>
2024-11-19 15:03:37 +0900

    add comment of article

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -14,38 +14,18 @@ import java.util.List;
 @Slf4j
 @RestController
 public class ArticleApiController {
-    /*@Autowired
-    private ArticleRepository articleRepository;*/
-
     @Autowired
     private ArticleService articleService;
-
-
-    /*@GetMapping("/api/articles")
-    public List<Article> index() {
-        return articleRepository.findAll();
-    }*/
 
     @GetMapping("/api/articles")
     public List<Article> index() {
         return articleService.index();
     }
 
-    /*@GetMapping("/api/articles/{id}")
-    public Article show(@PathVariable Long id) {
-        return articleRepository.findById(id).orElse(null);
-    }*/
-
     @GetMapping("/api/articles/{id}")
     public Article show(@PathVariable Long id) {
         return articleService.show(id);
     }
-
-    /*@PostMapping("/api/articles")
-    public Article create(@RequestBody ArticleForm dto) {
-        Article article = dto.toEntity();
-        return articleRepository.save(article);
-    }*/
 
     @PostMapping("/api/articles")
     public ResponseEntity<Article> create(@RequestBody ArticleForm dto) {
@@ -53,26 +33,6 @@ public class ArticleApiController {
         return (created != null)?ResponseEntity.status(HttpStatus.OK).body(created):
                                  ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
-
-    /*@PatchMapping("/api/articles/{id}")
-    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
-//        1. DTO -> 엔티티 변환
-        Article article = dto.toEntity();
-        log.info("id: {}, article:{}", id, article.toString());
-
-//        2. 타깃 조회
-        Article target = articleRepository.findById(id).orElse(null);
-
-//        3. 잘못된 요청 처리
-        if (target == null || id != article.getId()) {
-            log.info("wrong request. id: {}, article: {}", id, article.toString());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-//        4. 업데이트 및 정상 응답(200)하기
-        target.patch(article);
-        Article updated = articleRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
-    }*/
 
     @PatchMapping("/api/articles/{id}")
     public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
